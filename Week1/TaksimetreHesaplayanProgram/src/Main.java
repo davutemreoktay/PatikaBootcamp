439cd51a0c648d020c551037609a288320b08ae0
davutemreoktay <davutemreoktay@gmail.com>
2023-09-16 13:13:14 +0300

    KM Hesaplama

---
+++ Week1/TaksimetreHesaplayanProgram/src/Main.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        int km,startPrice=10;
+        double perKm=2.20,total;
+        Scanner input =new Scanner(System.in);
+        System.out.print("Mesafeyi Giriniz...");
+        km = input.nextInt();
+        total=km*perKm;
+        System.out.println(total);
+        total+=startPrice;
+
+        total=(total<20)?20:total;
+        System.out.println("Toplam Tutar:"+ total);
+
+    }
+}
