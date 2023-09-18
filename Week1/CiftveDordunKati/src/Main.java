3f0ea701a5e0e67c8334a2e8fdbdb8d45a2204f7
davutemreoktay <davutemreoktay@gmail.com>
2023-09-18 18:35:04 +0300

    DördünKatı

---
+++ Week1/CiftveDordunKati/src/Main.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+
+        int n;
+        int total=0;
+
+        Scanner inp=new Scanner(System.in);
+
+
+        do{
+            System.out.print("Sayı Giriniz: ");
+            n=inp.nextInt();
+            if ( (n%2==0) && (n%4==0) ){
+                total += n;
+            }
+        } while(n>0 && !(n%2==1) );
+        System.out.print("Toplam: "+ total);
+
+    }
+}
